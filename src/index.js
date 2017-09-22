8a65f92f47eb8935829de397f89454b9b8decbe9
Mauro Titimoli <mauro@jumpn.com>
2017-09-22 20:20:47 -0300

    chore(src): Remove deprecated file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ export {default as allByName} from "./allByName.js";
 export {default as chain} from "./chain.js";
 export {default as createDeferred} from "./createDeferred.js";
 export {default as promiseAllByName} from "./promiseAllByName.js";
-export {default as promisify} from "./promisify.js";
 export {default as promisifyFunction} from "./promisifyFunction.js";
 export {default as promisifyObject} from "./promisifyObject.js";
 export {default as track} from "./track.js";
