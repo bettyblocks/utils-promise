9028181e3325206abec99396a27a4f93b7ce3fe2
Mauro Titimoli <mauro@jumpn.com>
2017-09-22 19:40:15 -0300

    build(src): Now index is autogenerated using create-index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 // @flow
 
-export {default as allByName} from "./allByName";
-export {default as chain} from "./chain";
-export {default as createDeferred} from "./createDeferred";
-export {default as promisifyFunction} from "./promisifyFunction";
-export {default as promisifyObject} from "./promisifyObject";
-export {default as track} from "./track";
-export {default as try} from "./try";
+// @create-index
+
+export {default as allByName} from "./allByName.js";
+export {default as chain} from "./chain.js";
+export {default as createDeferred} from "./createDeferred.js";
+export {default as promiseAllByName} from "./promiseAllByName.js";
+export {default as promisify} from "./promisify.js";
+export {default as promisifyFunction} from "./promisifyFunction.js";
+export {default as promisifyObject} from "./promisifyObject.js";
+export {default as track} from "./track.js";
+export {default as try} from "./try.js";
